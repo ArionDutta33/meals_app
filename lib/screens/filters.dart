473add0a140769d04f35346dae2f5e77e3db48a8
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-07-28 13:40:14 +0530

    Lactose and vegetarian filters added

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -10,6 +10,9 @@ class FilterScreen extends StatefulWidget {
 
 class _FilterScreenState extends State<FilterScreen> {
   bool _gluttenFree = false;
+  bool _lactoseFree = false;
+  bool _vegetarian = false;
+  final bool _vegan = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +54,55 @@ class _FilterScreenState extends State<FilterScreen> {
             ),
             activeColor: Theme.of(context).colorScheme.tertiary,
             contentPadding: const EdgeInsets.only(left: 34, right: 22),
-          )
+          ),
+          SwitchListTile(
+            value: _lactoseFree,
+            onChanged: (value) {
+              setState(() {
+                _lactoseFree = value;
+              });
+            },
+            title: Text(
+              "Lactose free",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            subtitle: Text(
+              "Only include lacotse free meals",
+              style: Theme.of(context)
+                  .textTheme
+                  .labelMedium!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
+          SwitchListTile(
+            value: _vegetarian,
+            onChanged: (value) {
+              setState(() {
+                _vegetarian = value;
+              });
+            },
+            title: Text(
+              "Vegetarian",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            subtitle: Text(
+              "Only include vegetarian meals",
+              style: Theme.of(context)
+                  .textTheme
+                  .labelMedium!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
         ],
       ),
     );
